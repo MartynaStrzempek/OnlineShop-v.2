16034dcfc7720cd81e831a34e2ef8dafb9fbb8ce
MartynaStrzempek <martyna.strzempek@gmail.com>
2017-10-04 11:15:22 +0200

    New directory MainPage

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import MainPage from "./Layouts/MainPage";
+import MainPage from "./Layouts/MainPage/MainPage";
 
 ReactDOM.render(<MainPage />, document.getElementById('root'));
 registerServiceWorker();
