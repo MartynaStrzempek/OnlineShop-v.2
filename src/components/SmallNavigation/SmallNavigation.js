f8ac11532a9f03c6104389d8c7374b6d845906e0
MartynaStrzempek <martyna.strzempek@gmail.com>
2017-10-12 09:46:14 +0200

    import Switch and Route

--- src/components/SmallNavigation/SmallNavigation.js
+++ src/components/SmallNavigation/SmallNavigation.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import './SmallNavigation.css'
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-import { Miss, Match } from 'react-router-native';
+import { Route, Switch } from 'react-router';
+import Home from './Home';
+import SignUpPage from './SignUpPage';
 
 export default class SmallNavigation extends Component {
     render() {
         return (
-            <section className="smallNav">
-                <div className="container">
-                    <nav>
-                        <ul>
-                            <li>Home</li>
-                            <li>Help</li>
-                            <li>Contact</li>
-                            <li>Sign In</li>
-                            <li>Sign Up</li>
-                            <li>Cart</li>
-                        </ul>
-                    </nav>
-                </div>
-            </section>
+            <div>
+                <Router>
+                    <section className="smallNav">
+                        <div className="container">
+                            <nav>
+                                <ul>
+                                    <li><Link to="/Home">Home</Link></li>
+                                    <li>Help</li>
+                                    <li>Contact</li>
+                                    <li>Sign In</li>
+                                    <li><Link to="/SignUpPage">Sign Up</Link></li>
+                                    <li>Cart</li>
+                                </ul>
+                                <Match exactly pattern="/Home" component={Home}/>
+                                <Match pattern="/SignUpPage" component={SignUpPage}/>
+                            </nav>
+                        </div>
+                    </section>
+                </Router>
+            </div>
 
         );
     }
